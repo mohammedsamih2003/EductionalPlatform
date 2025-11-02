43785d21b845bb38579a4b4b94d3120488361d51
mohammedsamih2003 <mhmdsamih49@gmail.com>
2025-11-02 21:55:01 +0200

    Implemented CustomUserDetails and CustomUserDetailsService for authentication

--- src/main/java/com/educationalplatform/repositories/UserRepository.java
+++ src/main/java/com/educationalplatform/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import com.educationalplatform.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.lang.ScopedValue;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     List<User> findByRole(Role role);
+
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/com/educationalplatform/security/model/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.educationalplatform.security.model;
+
+import com.educationalplatform.entity.User;
+import com.educationalplatform.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new CustomUserDetails(user);
+    }
+}
