fe88630cf07e83232ee6f7b3d4782514d69d9283
mohammedsamih2003 <mhmdsamih49@gmail.com>
2025-11-10 21:38:04 +0200

    Created DTOs: LoginRequest, AuthResponse, and RegisterRequest for authentication process

---
+++ src/main/java/com/educationalplatform/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.educationalplatform.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+    private String token;
+    private String message;
+
+    public AuthResponse(String token, String message) {
+        this.token = token;
+        this.message = message;
+    }
+}
